d13079e57046ea26fd3269e3007dac174e0356da
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-12-16 18:00:11 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by kenjimaeda on 16/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
